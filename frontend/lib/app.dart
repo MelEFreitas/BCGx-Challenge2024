12a5f1866b7cff8bec13785ba09979a247b9b35e
Matheus Baptistella <mbapts@gmail.com>
2024-09-30 18:13:35 -0300

    feat: add initial dependencies and helper files

---
+++ frontend/lib/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [],
+      child: const Placeholder(),
+    );
+  }
+}
